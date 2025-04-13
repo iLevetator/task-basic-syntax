ae3597014d883350caf946107ae4914aaebb6c69
Levetator <iLevetator@yandex.ru>
2025-04-13 17:18:06 +0300

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,20 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    for (let i = 0; i < str.length; i++) {
+        if (i != str.length && roman[str[i]] < roman[str[i + 1]]) {
+            result -= roman[str[i]];
+        } else {
+            result += roman[str[i]];
+        }
+    }
     return result;
 }
